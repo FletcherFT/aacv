16a32fb7a86a32b74fbf6e375aa4344743631ecd
FletcherFT <Fletcher.Thompson@utas.edu.au>
2017-05-11 19:25:04 +1000

    Modified test.sh for any hostname/URI

--- launch/test.sh
+++ launch/test.sh
@@ -1,4 +1,4 @@
-export ROS_HOSTNAME=fft-Latitude-E5450.local
-export ROS_MASTER_UIR=http://fft-Latitude-E5450.local:11311
+export ROS_HOSTNAME=$HOSTNAME.local
+export ROS_MASTER_URI=http://$HOSTNAME.local:11311
 export ROSLAUNCH_SSH_UNKNOWN=1
 roslaunch rc_aacv remote_aacv.launch
